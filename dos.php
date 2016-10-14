1e5e92d6072311b06fc3bcccbd5785c541a32e63
jaumealonso21 <jaumealonso@hotmail.com>
2016-10-14 12:14:25 +0200

    Version 1.5 (final?)

--- dos.php
+++ dos.php
@@ -11,9 +11,23 @@ $sexo = $_REQUEST['sexo'];
 print ($sexo);
 
 $extras = $_REQUEST['extras'];
-foreach($extras as $extra)
-print("$extra<BR>\n");
-
+foreach($extras as $extra) {
+    print("$extra<BR>\n");
+}
+$fichero = $_REQUEST['fichero'];
+print (" $fichero<br />\n");
+$username = $_REQUEST['username'];
+print (" $username<br />\n");
+$clave = $_REQUEST['clave'];
+print (" $clave<br />\n");
+$enviar = $_REQUEST['enviar'];
+if ($enviar) { print ("Se ha pulsado el botón de enviar<br />\n"); }
+$color = $_REQUEST['color'];
+print ("$color<br />\n");
+$idiomas = $_REQUEST['idiomas'];
+foreach ($idiomas as $idioma) { print ("$idioma<BR>\n"); }
+$comentario = $_REQUEST['comentario'];
+print ("$comentario<br />\n");
 /*$actualizar = $_REQUEST['actualizar'];
 if($actualizar)
   print("Se han actualizado los datos");
--- uno.php
+++ uno.php
@@ -1,15 +1,36 @@
 <HTML>
 <BODY>
-<FORM ACTION="dos.php" METHOD="POST">
-Edad: <INPUT TYPE="text" NAME="edad">
-<INPUT TYPE="submit" VALUE="aceptar">
-<INPUT TYPE="text" NAME="cadena" VALUE="valor por defecto" SIZE="20"><br>
-Sexo:<INPUT TYPE="radio" NAME="sexo" VALUE="M" CHECKED>Mujer
-	<INPUT TYPE="radio" NAME="sexo" VALUE="H">Hombre
-<INPUT TYPE="checkbox" NAME="extras[]" VALUE="garaje" CHECKED>Garaje
-<INPUT TYPE="checkbox" NAME="extras[]" VALUE="piscina">Piscina
-<INPUT TYPE="checkbox" NAME="extras[]" VALUE="jardin">Jardín
-<INPUT TYPE="button" NAME="actualizar" VALUE="Actualizar datos" disabled="disabled">
-</FORM>
+    <FORM ACTION="dos.php" METHOD="POST" enctype="multipart/form-data">
+        Edad: <INPUT TYPE="text" NAME="edad">
+        <INPUT TYPE="submit" VALUE="aceptar" disabled="disabled" />
+        <INPUT TYPE="text" NAME="cadena" VALUE="valor por defecto" SIZE="20" /><br />
+        Sexo:<INPUT TYPE="radio" NAME="sexo" VALUE="M" CHECKED>Mujer
+                <INPUT TYPE="radio" NAME="sexo" VALUE="H">Hombre
+        <INPUT TYPE="checkbox" NAME="extras[]" VALUE="garaje" CHECKED>Garaje
+        <INPUT TYPE="checkbox" NAME="extras[]" VALUE="piscina">Piscina
+        <INPUT TYPE="checkbox" NAME="extras[]" VALUE="jardin">Jardín
+        <INPUT TYPE="button" NAME="actualizar" VALUE="Actualizar datos" disabled="disabled"><br />
+        <input type="file" name="fichero" /><br />
+        <INPUT TYPE='hidden' NAME='username' VALUE='$usuario' /><br />
+        Contraseña: <INPUT TYPE="password" NAME="clave" /><br />
+        <INPUT TYPE="submit" NAME="enviar" VALUE="Enviar datos"><br />
+        Color:
+        <SELECT NAME="color">
+            <OPTION VALUE="rojo" SELECTED>Rojo
+            <OPTION VALUE="verde">Verde
+            <OPTION VALUE="azul">Azul
+        </SELECT>
+        Idiomas:
+        <SELECT MULTIPLE SIZE="3" NAME="idiomas[]">
+            <OPTION VALUE="ingles" SELECTED>Inglés
+            <OPTION VALUE="frances">Francés
+            <OPTION VALUE="aleman">Alemán
+            <OPTION VALUE="holandes">Holandés
+        </SELECT><br />
+        Comentario:
+        <TEXTAREA COLS="50" ROWS="4" NAME="comentario">
+        Este libro me parece ...
+        </TEXTAREA>
+    </FORM>
 </BODY>
 </HTML>
